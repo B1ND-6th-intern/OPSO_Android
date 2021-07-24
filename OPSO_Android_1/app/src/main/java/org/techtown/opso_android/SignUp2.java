25fda31725cf175d0982041f0ecd8a49174c50b8
snack655 <cmjcmj1230@gmail.com>
2021-07-24 14:10:28 +0900

    2021-07-24 Complete UI Sign
    Sign Up and Sign In UI
    Complete

--- OPSO_Android_1/app/src/main/java/org/techtown/opso_android/SignUp2.java
+++ OPSO_Android_1/app/src/main/java/org/techtown/opso_android/SignUp2.java
@@ -7,10 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class SignUp2 extends AppCompatActivity {
 
     Button btn_SignUp;
+    Button btn_cerEmail;
     ImageButton btn_Sign_Up1_Back;
 
     @Override
@@ -18,8 +20,10 @@ public class SignUp2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up2);
 
+        btn_cerEmail = findViewById(R.id.btn_cerEmail);
         btn_Sign_Up1_Back = findViewById(R.id.btn_Sign_Up1_Back);
         btn_SignUp = findViewById(R.id.btn_SignUp);
+        
         btn_SignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,6 +37,13 @@ public class SignUp2 extends AppCompatActivity {
             public void onClick(View v) {
                 Intent gotoSignUp1Intent = new Intent(getApplicationContext(), SignUp1.class);
                 startActivity(gotoSignUp1Intent);
+            }
+        });
+        
+        btn_cerEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SignUp2.this, "이메일 인증 입니다!", Toast.LENGTH_SHORT).show();
             }
         });
     }
